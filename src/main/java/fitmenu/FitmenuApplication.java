5949bd74edfd34ca78533861bb6bd4d0180a02d7
Mateusz K <kakol_mateusz@o2.pl>
2019-09-01 00:43:21 +0200

    init

---
+++ src/main/java/fitmenu/FitmenuApplication.java
@@ -0,0 +1,13 @@
+package fitmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmenuApplication.class, args);
+	}
+
+}
